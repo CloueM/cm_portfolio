bf62860462649a737985059f25526b5ea034f90e
Cloue Macz <maczyepez@gmail.com>
2025-05-31 20:19:24 -0700

    Setup main page

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+    return(
+        <section id='hero' className='relative overflow-hidden'>
+            <div className='absolute top-0 left-0 z-10'>
+                <img scr="/images/bg.png" alt="background"/>
+            </div>
+        </section>
+    )
+}
+
+
+export default Hero
