0a67890546cb57b940e44b4cea1faaa67d87fd4b
CM <maczyepez@gmail.com>
2025-06-01 01:49:13 -0700

    Edit button.jsx style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,15 +4,12 @@ const Button = ({ text, className, id }) => {
   return (
     <a className={`${className ?? ''} cta-wrapper`}>
         <div className='cta-button group'>
-            <div className='bg-circle'>
-                <p className='text'>
-                    See my Work
-                </p>
-                <div className='arrow-wrapper'>
-                    <img src='/images/arrow-down.svg' alt='arrow'/>
-                </div>
+            <div className='bg-circle'/>
+            <p className='text'>{text}</p>
+            <div className='arrow-wrapper'>
+                <img src='/images/arrow-down.svg' alt='arrow'/>
             </div>
-        </div>
+            </div>
     </a>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+import autoprefixer from 'autoprefixer'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    autoprefixer()
   ],
 })
