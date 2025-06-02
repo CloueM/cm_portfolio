0111d6a55a788d50bfd9507bbaa010100e0ceac5
CM <maczyepez@gmail.com>
2025-06-01 19:42:21 -0700

    Add/Edit 3D Model Lightings

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -3,14 +3,13 @@ import {Canvas} from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei';
 import { useMediaQuery } from 'react-responsive';
 import {Room} from './Room.jsx'
+import HeroLights from './HeroLights.jsx';
 const HeroExperience = () => {
     const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
     const isMoblie = useMediaQuery({query: '(max-width: 768px)'});
   return (
     <Canvas camera={{position: [0,0,15], fov:45 }}>
-        <ambientLight intensity={0.2} color="#1a1a40"/>
-        <directionalLight position={[5,5,5]} intensity={1}/>
-        
+
         <OrbitControls
             enablePan={false}
             enableZoom={!isTablet}
@@ -19,7 +18,14 @@ const HeroExperience = () => {
             minPolarAngle={Math.PI/5}
             maxPolarAngle={Math.PI/2}
         />
-        <Room/>
+        <HeroLights/>
+        <group
+            scale={isMoblie? 0.7:1}
+            position={[0,-3.5,0]}
+            rotation={[0, -Math.PI/4,0]}
+        >
+            <Room/>
+        </group>
     </Canvas>
   )
 }
